b826457fb9bde30493894e67e83e1f02e4635955
Les <phosphor.renkei@gmail.com>
2026-01-03 04:26:49 -0800

    Display blueprint icons in UI by adding `IconUrl` property to `Blueprint` model and integrating `AsyncImageLoader.Avalonia` package. Update layout to include icon and adjust styling.

--- EVE-NEIC.App/Models/Blueprint.cs
+++ EVE-NEIC.App/Models/Blueprint.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 
 namespace EVE_NEIC.App.Models;
 
@@ -10,6 +9,6 @@ public class Blueprint
     public int GroupId { get; set; }
     public string GroupName { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    //public List<Material> Materials { get; set; } = new();
     public ObservableCollection<Material> Materials { get; set; } = new();
+    public string IconUrl => $"https://images.evetech.net/types/{TypeId}/bp?size=64";
 }
